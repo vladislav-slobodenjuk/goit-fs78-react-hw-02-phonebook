1534e47b2318c94c7fca6cc7e1558dbc6570b6b6
vladislav-slobodenjuk <vladjun@gmail.com>
2023-06-25 02:03:31 +0300

    base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleInputChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, contacts } = this.state;
+    this.setState({ contacts: [...contacts, { name, id: nanoid() }] });
+    this.setState({ name: '' });
+  };
+
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        {/* <ContactForm
+          name={this.state.name}
+          onInputChange={this.handleInputChange}
+          onSubmit={this.handleSubmit}
+        /> */}
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+
+        <h2>Contacts</h2>
+        {/* <Filter .../> */}
+        {/* <ComponentList .../> */}
+
+        <ul>
+          {contacts.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
